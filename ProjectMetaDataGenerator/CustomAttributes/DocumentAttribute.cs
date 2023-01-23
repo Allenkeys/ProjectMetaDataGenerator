284267fa7d6b4a4afbfc139c97cde8f893a403b5
Joshua Eze <ezejoshuac@gmail.com>
2023-01-23 23:18:17 +0100

    Added a custom attribute class.

---
+++ ProjectMetaDataGenerator/CustomAttributes/DocumentAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMetaDataGenerator.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    internal class DocumentAttribute : Attribute
+    {
+        public string Description { get; set; }
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public DocumentAttribute(string description, string input = null, string output = null)
+        {
+            Description = description;
+            Input = input;
+            Output = output;
+        }
+    }
+}
